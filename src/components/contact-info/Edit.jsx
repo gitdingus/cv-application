e62db99fa70d70c6de7f506a4ec583ede77851e9
Josh S <aaabccc87@protonmail.com>
2022-09-30 13:03:12 -0400

    Setup events for state changes

--- src/components/contact-info/Edit.jsx
+++ src/components/contact-info/Edit.jsx
@@ -4,33 +4,81 @@ import styles from './edit.module.css';
 class Edit extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      name: '',
+      email: '',
+      phone: '',
+      phoneType: 'cell',
+      phoneList: [],
+    };
+
+    this.nameChanged = this.nameChanged.bind(this);
+    this.emailChanged = this.emailChanged.bind(this);
+    this.phoneInputChanged = this.phoneInputChanged.bind(this);
+    this.phoneTypeChanged = this.phoneTypeChanged.bind(this);
+    this.addPhone = this.addPhone.bind(this);
+  }
+
+  nameChanged(e) {
+    console.log('name changed');
+    this.setState({
+      name: e.target.value,
+    });
+  }
+
+  emailChanged(e) {
+    console.log('email changed');
+    this.setState({
+      email: e.target.value,
+    });
+  }
+
+  phoneInputChanged(e) {
+    console.log('phone input changed');
+    this.setState({
+      phone: e.target.value,
+    });
+  }
+
+  phoneTypeChanged(e) {
+    console.log('phone type changed');
+    this.setState({
+      phoneType: e.target.value,
+    });
+  }
+
+  addPhone() {
+    console.log('add phone');
   }
 
   render() {
-    console.log(`styles ${styles}`);
+    const {
+      name, email, phone, phoneType,
+    } = this.state;
     return (
       <fieldset className={styles.contactInfo}>
         <legend>Contact Information</legend>
         <label htmlFor="name">
           <span>Name</span>
-          <input id="name" type="text" />
+          <input id="name" type="text" onChange={this.nameChanged} value={name} />
         </label>
         <label htmlFor="email">
           <span>Email</span>
-          <input id="email" type="text" />
+          <input id="email" type="text" onChange={this.emailChanged} value={email} />
         </label>
         <div className="phone-numbers">
           <label htmlFor="phone">
             <span>Phone</span>
-            <input id="phone" type="text" />
+            <input id="phone" type="text" onChange={this.phoneInputChanged} value={phone} />
           </label>
-          <select id="phone-type">
+          <select id="phone-type" onChange={this.phoneTypeChanged} value={phoneType}>
             <option value="cell">Cell</option>
-            <option value="home" selected>Home</option>
+            <option value="home">Home</option>
             <option value="work">Work</option>
             <option value="other">Other</option>
           </select>
-          <button type="button">Add</button>
+          <button type="button" onClick={this.addPhone}>Add</button>
         </div>
       </fieldset>
     );

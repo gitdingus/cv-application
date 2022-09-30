cd23f747f3b902e731069595dce976eff4e60334
Josh S <aaabccc87@protonmail.com>
2022-09-30 12:35:30 -0400

    Rough markup and layout for Edit component in ContactInfo component

--- src/components/contact-info/ContactInfo.jsx
+++ src/components/contact-info/ContactInfo.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
+import Edit from './Edit.jsx';
 import styles from './contact-info.css';
+
+class ContactInfo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Edit />
+    );
+  }
+}
+export default ContactInfo;
--- src/components/contact-info/Edit.jsx
+++ src/components/contact-info/Edit.jsx
@@ -1,1 +1,40 @@
-import styles from './edit.css';
+import React from 'react';
+import styles from './edit.module.css';
+
+class Edit extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    console.log(`styles ${styles}`);
+    return (
+      <fieldset className={styles.contactInfo}>
+        <legend>Contact Information</legend>
+        <label htmlFor="name">
+          <span>Name</span>
+          <input id="name" type="text" />
+        </label>
+        <label htmlFor="email">
+          <span>Email</span>
+          <input id="email" type="text" />
+        </label>
+        <div className="phone-numbers">
+          <label htmlFor="phone">
+            <span>Phone</span>
+            <input id="phone" type="text" />
+          </label>
+          <select id="phone-type">
+            <option value="cell">Cell</option>
+            <option value="home" selected>Home</option>
+            <option value="work">Work</option>
+            <option value="other">Other</option>
+          </select>
+          <button type="button">Add</button>
+        </div>
+      </fieldset>
+    );
+  }
+}
+
+export default Edit;

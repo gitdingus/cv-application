3475b9330d9e16b8e3d150ef76a35512708d5031
Josh S <aaabccc87@protonmail.com>
2022-10-03 14:00:14 -0400

    Ability to take changes from Edit component

--- src/components/contact-info/ContactInfo.jsx
+++ src/components/contact-info/ContactInfo.jsx
@@ -5,11 +5,42 @@ import styles from './contact-info.css';
 class ContactInfo extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      mode: 'edit',
+      name: '',
+      email: '',
+      phoneList: [],
+    };
+
+    this.updateContactInfo = this.updateContactInfo.bind(this);
+  }
+
+  updateContactInfo(recievedState) {
+    this.setState({ name: recievedState.name });
+    this.setState({ email: recievedState.email });
+    this.setState({ phoneList: recievedState.phoneList });
+    this.setState({ mode: 'display' });
+  }
+
   }
 
   render() {
+    const {
+      name,
+      email,
+      phoneList,
+      mode,
+    } = this.state;
+
+
     return (
-      <Edit />
+      <Edit
+        name={name}
+        email={email}
+        phoneList={phoneList}
+        updateContactInfo={this.updateContactInfo}
+      />
     );
   }
 }

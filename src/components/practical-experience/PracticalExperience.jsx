ada7bbf01bd63a99a0665a4e1787617815b78cec
Josh S <aaabccc87@protonmail.com>
2022-10-10 16:39:54 -0400

    Keep track of work experience in state

--- src/components/practical-experience/PracticalExperience.jsx
+++ src/components/practical-experience/PracticalExperience.jsx
@@ -4,11 +4,25 @@ import styles from './practical-experience.module.css';
 import '../app.css';
 
 class PracticalExperience extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      workExperience: [],
+    };
+  }
+
+  addWorkExperience(data) {
+    this.setState((prevState) => ({
+      workExperience: prevState.workExperience.concat([data]),
+    }));
+  }
+
   render() {
     return (
       <div className="cv-section">
         <h1 className="cv-section-header">Practical Experience</h1>
-        <Edit />
+        <Edit addWorkExperience={this.addWorkExperience} />
       </div>
     );
   }

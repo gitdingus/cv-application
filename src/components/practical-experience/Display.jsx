26902d98c593355a466e7107b340c2ecfb44cd94
Josh S <aaabccc87@protonmail.com>
2022-10-11 15:44:16 -0400

    Markup, styling, and ability to edit/remove work experience

--- src/components/practical-experience/Display.jsx
+++ src/components/practical-experience/Display.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './display.module.css';
+import editButton from '../edit.svg';
+import deleteButton from '../trash-can-outline.svg';
 
 class Display extends React.Component {
   render() {
+    const { workExperience, removeWorkExperience, editWorkExperience } = this.props;
     return (
-      <div>
-
+      <div className={styles.practicalExperience}>
+        <ul>
+          {
+            workExperience.map((item) => (
+              <li key={item.uuid} className={styles.job}>
+                <div className={styles.jobHeadline}>
+                  <span>{item.beginDate}</span>
+                  <span>-</span>
+                  <span>{item.endDate}</span>
+                  <span className={styles.companyName}>{item.companyName}</span>
+                  <span className={styles.jobTitle}>{item.jobTitle}</span>
+                  <button className="svg-button" type="button" onClick={() => editWorkExperience(item.uuid)}><img src={editButton} alt="Edit Work Experience" /></button>
+                  <button className="svg-button" type="button" onClick={() => removeWorkExperience(item.uuid)}><img src={deleteButton} alt="Delete Work Experience" /></button>
+                </div>
+                <ul className={styles.responsibility}>
+                  {
+                    item.responsibilities.map((responsibility) => (
+                      <li
+                        key={responsibility.uuid}
+                      >
+                        {responsibility.responsibility}
+                      </li>
+                    ))
+                  }
+                </ul>
+              </li>
+            ))
+          }
+        </ul>
       </div>
     );
   }
@@ -22,10 +53,14 @@ Display.propTypes = {
       responsibility: PropTypes.string,
     })),
   })),
+  removeWorkExperience: PropTypes.func,
+  editWorkExperience: PropTypes.func,
 };
 
 Display.defaultProps = {
   workExperience: [],
+  removeWorkExperience: () => {},
+  editWorkExperience: () => {},
 };
 
 export default Display;

53aae101382a1183de490adca8dad1cca69d9378
Josh S <aaabccc87@protonmail.com>
2022-10-14 15:56:12 -0400

    Create markup for resume from supplied data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ContactInfo from './contact-info/ContactInfo.jsx';
 import EducationExperience from './education-experience/EducationExperience.jsx';
 import PracticalExperience from './practical-experience/PracticalExperience.jsx';
-
 import './app.css';
 
 class App extends React.Component {
@@ -13,6 +12,7 @@ class App extends React.Component {
       contactInfo: null,
       educationExperience: null,
       practicalExperience: null,
+      mode: 'edit',
     };
 
     this.receiveContactInfo = this.receiveContactInfo.bind(this);
@@ -22,21 +22,18 @@ class App extends React.Component {
   }
 
   receiveContactInfo(data) {
-    console.log('received contact info');
     this.setState({
       contactInfo: data,
     });
   }
 
   receiveEducationExperience(data) {
-    console.log('recieved education experience');
     this.setState({
       educationExperience: data,
     });
   }
 
   receivePracticalExperience(data) {
-    console.log('recieved practical experience');
     this.setState({
       practicalExperience: data,
     });
@@ -44,20 +41,93 @@ class App extends React.Component {
 
   createCv() {
     const { contactInfo, educationExperience, practicalExperience } = this.state;
-    console.log(contactInfo);
-    console.log(educationExperience);
-    console.log(practicalExperience);
+    if (
+      contactInfo === null
+      || educationExperience === null
+      || practicalExperience === null
+    ) {
+      return;
+    }
+
+    this.setState({
+      mode: 'display',
+    });
   }
 
   render() {
+    const {
+      contactInfo, educationExperience, practicalExperience, mode,
+    } = this.state;
+
+    if (mode === 'edit') {
+      return (
+        <div>
+          <ContactInfo sendContactInfo={this.receiveContactInfo} />
+          <EducationExperience sendEducationExperience={this.receiveEducationExperience} />
+          <PracticalExperience sendPracticalExperience={this.receivePracticalExperience} />
+          <button type="button" onClick={this.createCv}>Create CV</button>
+        </div>
+      );
+    }
+
     return (
       <div>
-        <ContactInfo sendContactInfo={this.receiveContactInfo} />
-        <EducationExperience sendEducationExperience={this.receiveEducationExperience} />
-        <PracticalExperience sendPracticalExperience={this.receivePracticalExperience} />
-        <button type="button" onClick={this.createCv}>Create CV</button>
+        <div className="contact-info">
+          <p>{contactInfo.name}</p>
+          <p>{contactInfo.email}</p>
+          {
+            contactInfo.phoneList.map((phone) => (
+              <p key={phone.uuid}>
+                <span>{phone.type}</span>
+                <span>{phone.number}</span>
+              </p>
+            ))
+          }
+        </div>
+        <div className="education-experience">
+          {
+            educationExperience.map((exp) => (
+              <p key={exp.uuid}>
+                <span>{exp.university}</span>
+                <span>{exp.areaOfStudy}</span>
+                <span>{exp.degreeEarned}</span>
+                <span>{exp.dateAcquired}</span>
+              </p>
+            ))
+          }
+        </div>
+        <div className="practical-experience">
+          {
+            practicalExperience.map((exp) => {
+              const {
+                beginDate, endDate, companyName, jobTitle, responsibilities,
+              } = exp;
+              return (
+                <div key={exp.uuid}>
+                  <p>
+                    <span>{beginDate}</span>
+                    <span>{endDate}</span>
+                    <span>{companyName}</span>
+                    <span>{jobTitle}</span>
+                  </p>
+                  <ul>
+                    {
+                      responsibilities.map((resp) => {
+                        const { uuid, responsibility } = resp;
+                        return (
+                          <li key={uuid}>
+                            {responsibility}
+                          </li>
+                        );
+                      })
+                    }
+                  </ul>
+                </div>
+              );
+            })
+          }
+        </div>
       </div>
-
     );
   }
 }
